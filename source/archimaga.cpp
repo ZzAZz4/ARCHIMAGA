6f1b35136eb9c45780170e2af1cf822ecd525a62
esteb <esteban.villacorta@utec.edu.pe>
2024-05-20 18:50:41 -0500

    reformat

--- source/archimaga.cpp
+++ source/archimaga.cpp
@@ -1,9 +1,10 @@
 #include <string>
 
 #include "archimaga/archimaga.hpp"
+
 #include <SDL2/SDL.h>
-#include <imgui.h>
 #include <fmt/core.h>
+#include <imgui.h>
 
 exported_class::exported_class()
     : m_name {fmt::format("{}", "archimaga")}
